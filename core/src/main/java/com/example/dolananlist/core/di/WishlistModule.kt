c5f07b325b5a25ee6281fb28552d1f7d8a9b53e1
Izznakim <IzzuddinNaufalHakim@gmail.com>
2022-08-06 19:07:57 +0700

    Menerapkan obfuscation pada aplikasi dan menerapkan encryption pada database sudah selesai.

--- core/src/main/java/com/example/dolananlist/core/di/WishlistModule.kt
+++ core/src/main/java/com/example/dolananlist/core/di/WishlistModule.kt
@@ -6,6 +6,8 @@ import com.example.dolananlist.core.data.source.local.LocalDataSource
 import com.example.dolananlist.core.data.source.local.room.WishlistDatabase
 import com.example.dolananlist.core.domain.repository.IWishlistRepository
 import com.example.dolananlist.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -13,10 +15,12 @@ import org.koin.dsl.module
 val databaseModule = module {
     factory { get<WishlistDatabase>().wishlistDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("inaki".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             WishlistDatabase::class.java, "Game.db"
-        ).build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
