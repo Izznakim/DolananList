4f47b10f4145bc0052af0c55d70e1a7eeb13af2b
Izznakim <IzzuddinNaufalHakim@gmail.com>
2022-08-08 08:12:34 +0700

    Menerapkan certificate pinning selesai.

--- core/src/main/java/com/example/dolananlist/core/di/CoreModule.kt
+++ core/src/main/java/com/example/dolananlist/core/di/CoreModule.kt
@@ -5,14 +5,21 @@ import com.example.dolananlist.core.data.GameRepository
 import com.example.dolananlist.core.data.source.remote.RemoteDataSource
 import com.example.dolananlist.core.data.source.remote.retrofit.ApiService
 import com.example.dolananlist.core.domain.repository.IGameRepository
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = "api.rawg.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/Vt5/77IBRU8Ic76wffoVpn2hrTRotDK+cuASoGoEzS0=")
+            .add(hostname, "sha256/hS5jJ4P+iQUErBkvoWBQOd1T7VOAYlOVegvv1iMzpxA=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=").build()
         val loggingInterceptor = if (BuildConfig.DEBUG) {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         } else {
@@ -20,6 +27,9 @@ val networkModule = module {
         }
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
